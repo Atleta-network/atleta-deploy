4c41c0779cd718f1531077b726acc61e56ad96a0
semeniak97mf <n.semeniak@moneyfactory.io>
2024-09-03 10:32:22 +0300

    Optimize code for telemetry tasks of testnet

--- utils/telemetry/prometheus/generate_prometheus_config.sh
+++ utils/telemetry/prometheus/generate_prometheus_config.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-cat <<EOF > prometheus_template.yml
-global:
-  scrape_interval: 15s
-
-scrape_configs:
-  - job_name: 'prometheus'
-    static_configs:
-      - targets: ['localhost:9090']
-  - job_name: 'devnet'
-    static_configs:
-      - targets: ['${DEVNET_HOST}:9615', '${DEVNET_HOST}:9100', '${DEVNET_HOST}:9256']
-        labels:
-          host: '${DEVNET_HOST}'
-EOF
-
 for i in {1..16}; do
   host_key="VALIDATOR_${i}_HOST"
 
