e6d3d466e8c26a5692aed6091c4db955b5e0ca86
semeniak97mf <n.semeniak@moneyfactory.io>
2024-10-28 18:13:19 +0300

    Add missing containers for wipe mainnet 3

--- mainnet/wipe/wipe.sh
+++ mainnet/wipe/wipe.sh
@@ -9,21 +9,14 @@ source cleanup.env
 : "${container_promtail:=}"
 
 maybe_cleanup_containers() {
-
-    if [ -z "$(docker ps -aq -f name="$container_atleta")" ]; then
-        echo "Required container $container_atleta not found. Exiting..."
-        exit 1
-    fi
-
     containers=("$container_atleta" "$container_process_exporter" "$container_promtail")
 
     for container in "${containers[@]}"; do
-        if [ "$(docker ps -aq -f name="$container")" ]; then
+        if [ -n "$container" ] && [ "$(docker ps -aq -f name="$container")" ]; then
             echo "Stopping and removing existing container $container..."
-            docker stop "$container"
-            docker rm "$container"
+            docker rm -f "$container" || echo "Failed to remove container $container"
         else
-            echo "Container $container not found, skipping..."
+            echo "Container $container not found or name is empty, skipping..."
         fi
     done
 }
